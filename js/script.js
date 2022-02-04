aaf35e080c9ec5baf09e194cc3056ec035137180
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-02-04 21:10:20 +0100

    added an event to the button getstart

--- js/script.js
+++ js/script.js
@@ -1,32 +1,29 @@
+const pageAppear = ()=>{
+    let body = document.getElementsByTagName("body")[0];
+    body.classList.add("fadeIn");
+}
 
 const checkDevice = ()=>{
     if(screen.width>450){
             let emptyPage = document.querySelector(".column");
             console.log(emptyPage);
             emptyPage.classList.remove("column");
-            emptyPage.classList.add("circular");
             emptyPage.classList.add("row");
             emptyPage.classList.add("justify_center");
             emptyPage.innerHTML="PAGINA NON DISPONIBILE PER DESKTOP/TABLET, RIPROVA DAL TUO DISPOSITIVO MOBILE";
-            //alert("Quest'app supporta solo i dispositivi mobile, riprova dal cellulare!");
 }
 else{
-    addEventListener("load",()=>{ 
-        let body = document.getElementById("body");
-        setTimeout(body.classList.add("fadeIn"),1000);
-    });
+        setTimeout(pageAppear(),1000);
 }
 }
-window.onresize = checkDevice();
+
 const buildPage1 = ()=>{
     let page = document.getElementById("body");
-    page.innerHTML="";
+    let div = document.createElement(div);
+    div.innerHTML="QUESTA E' LA PAGINA UNO!";
+    page.appendChild(div);
 }
 const menuClick = ()=>{
     let btn = document.getElementsByClassName("menu")[0];
     btn.classList.toggle("menuClick");
-}
-const fadeIn = () =>{
-    let page = document.getElementsByClassName("body1")[0];
-    page.classList.add("fadeIn");
 }
