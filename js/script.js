67fdb2ebe222dea755e58b1870976f7eacb598d0
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-02-07 19:58:44 +0100

    added the title of the page

--- js/script.js
+++ js/script.js
@@ -23,12 +23,13 @@ let div;
 const buildPage1 = ()=>{
     let page = document.getElementsByTagName("body")[0];
     page.classList.add("column");
-    page.classList.add("align_center");
+    //page.classList.add("align_center");
     page.innerHTML=`
-                    <div class="row mg-lft-25 mg-rgt-25 align_center container mg-top-5">
+                    <div class="row mg-lft-25px mg-rgt-25px container mg-top-5">
                         <div class="menu" onclick="menuClick();"></div>
                         <div class="login"></div> 
                     </div>
+                    <h1 class="circular mg-lft-25px mg-top-15 blue">Find your Desired Doctor</h1>
     `
 }
 const menuClick = ()=>{
