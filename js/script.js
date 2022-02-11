7f34749a82667aa6a25fbc927a6e7a31d441234f
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-02-11 12:09:38 +0100

    optimized the first part of category show logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const spec = ["heart","eye","teeth"];
 const doctors = [
     {name:"Dr.Stella Kane",cat:"heart",hospital:"Flower Hospitals"},
     {name:"Joseph Cart",cat:"teeth",hospital:"Flower Hospitals"},
@@ -50,19 +51,19 @@ const buildPage1 = ()=>{
                         <div class="circular blue --16px">Categories</div>
                         <div class="scroll_menu row mg-top-5">
                             <div class="cat_card">
-                                <div id="teeth" class="cat_btn teeth"></div>
+                                <div id="teeth" class="cat_btn teeth_"></div>
                                 <div class="cat_card_content circular --16px violet">
                                     <span class="cat_desc">Dental Surgeon</span>
                                 </div>
                             </div>
                             <div class="cat_card">
-                                <div id="heart" class="cat_btn heart"></div>
+                                <div id="heart" class="cat_btn heart_"></div>
                                 <div class="cat_card_content circular --16px violet">
                                     <span class="cat_desc">Heart Surgeon</span>
                                 </div>
                             </div>
                             <div class="cat_card">
-                            <div id="eye" class="cat_btn eye"></div>
+                            <div id="eye" class="cat_btn eye_"></div>
                                 <div class="cat_card_content circular --16px violet">
                                     <span class="cat_desc">Eye Specialist</span>
                                 </div>
@@ -71,7 +72,7 @@ const buildPage1 = ()=>{
                         <div class="doct_container">
                         </div>
                     </div>
-    `
+                    `
     addingEvent();
 }
 const menuClick = ()=>{
@@ -79,36 +80,44 @@ const menuClick = ()=>{
     btn.classList.toggle("menuClick");
 }
 const addingEvent=()=>{
-    let spec_list = [];
     let cat_btns = document.getElementsByClassName("cat_btn");
-    let type = ["tooth","heart","eye"];
     for (let item of cat_btns){
         item.addEventListener("click",()=>{
                 if (item.classList.contains("deactive")){ //qua ci vanno le istruzioni di eliminazione categoria
-                    spec_list.splice(spec_list.length-1,1);
-                    console.log("array con splice: "+spec_list)
-                    displayCats(spec_list);
+                    delCats(item.id);
+                    item.classList.toggle("deactive");
+                }else{
+                    displayCats(item.id);
+                    console.log(item.id);
+                    item.classList.toggle("deactive");
                 }
-                item.classList.toggle("deactive");
-                //spec_list.push(item.id.toString());
-                displayCats(item.id);
-                console.log(item.id);
             });
     }
 }
+/*const displayAll = ()=>{ 
+    doc = doctors.filter(doc => doc.cat == "heart");
+    renderDoctor(doc);
+}*/
 const displayCats = (specialization) =>{ //spec è un array contenente tutte le specializzazioni attive
     doc = doctors.filter(doc => doc.cat == specialization);
     console.log(doc);
     renderDoctor(doc);
 }
-
+const delCats = (specialization) =>{
+    let delDocs = document.getElementsByClassName(specialization);
+    for (i=0;i<=delDocs.length-1;i++){
+        delDocs[i].remove();
+    }
+}
 const renderDoctor = (docList)=>{
     let doctor_contain = document.querySelector(".doct_container");
-    doctor_contain.innerHTML="";
     for (i=0;i<=docList.length-1;i++){
         let doctor = document.createElement("div");
-        doctor.innerHTML=`<div>${docList[i].name}</div>
-                          <div>${docList[i].hospital}</div>`;
+        doctor.classList.add(docList[i].cat);
+        doctor.innerHTML=`<div class='${docList[i].cat}'>
+                            <div>${docList[i].name}</div>
+                            <div>${docList[i].hospital}</div>
+                          </div>`;
         doctor_contain.append(doctor);
     }
 }
